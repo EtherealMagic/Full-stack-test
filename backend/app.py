29c4f8d25d2b9bff4e2f5bd20aaedff2258519aa
Tobias Ivan Godoy <tobiasig2001@gmail.com>
2026-07-04 18:30:28 -0300

    preparando publicación 3

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from db import crear_base, registrar_en_base, logear_en_base
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,8 @@ def login():
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port= int(os.environ.get("PORT", 5000)),
+        debug=True
+        )
